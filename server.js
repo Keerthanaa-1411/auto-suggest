fd3727ff6611e047758b51297b03518e62fed51b
Keerthanaa-1411 <24wh1a05bp@bvrithyderabad.edu.in>
2026-07-04 09:20:11 +0530

    FIX

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express =require('express');
 const app = express();
 const port = process.env.PORT || 3002;
 //web server
-app.use(express.static("frontend"));
+app.use(express.static("Frontend"));
 app.listen(port ,function(){
     console.log("Successfully running at http:/localhost:"+port);
 
